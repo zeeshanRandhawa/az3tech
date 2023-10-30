bccc9b3441894eca2c66a7c90b5a2de12fc49444
Hammad Hassan <125253931+hh139@users.noreply.github.com>
2023-10-30 11:40:44 +0500

    Update db.config.ts

--- util/db.config.ts
+++ util/db.config.ts
@@ -9,10 +9,10 @@ export const sequelize: any = new Sequelize(
     port: process.env.IS_PROD === "true" ? parseInt(process.env.PG_PORT_PROD!) : parseInt(process.env.PG_PORT_DEV!),
     dialect: "postgres",
     pool: {
-        max: 20,
+        max: 15,
         min: 0,
-        acquire: 90000,
-        idle: 5000
+        acquire: 60000,
+        idle: 3000
     }
     , dialectOptions: process.env.IS_PROD === "true" ? {
         ssl: {
